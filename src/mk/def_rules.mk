a39b5047d59a79e20916ee78dc64851dffdbbba3
VyshnaviKB <848pallavi@gmail.com>
2026-06-08 11:27:22 +0530

    updated cmd_to_cmd_receipe

--- src/mk/def_rules.mk
+++ src/mk/def_rules.mk
@@ -1152,7 +1152,7 @@ define CMD_TO_CMD_RECIPE =
 	$(eval PRDLIB = $(CMD_PRDLIB))
 	$(eval d = $($@_d))
 	@$(call echo_cmd,"=== Creating command [$(notdir $<)] in $(call ESCAPE_FOR_RECIPE,$(OBJLIB))")
-	$(eval crtcmd := CRTCMD CMD($(call ESCAPE_FOR_RECIPE,$(OBJLIB))/$(basename $(@F))) srcstmf('$<') PGM($(if $(CMD_PGM_LIB),$(CMD_PGM_LIB),$(call ESCAPE_FOR_RECIPE,$(OBJLIB)) $(CRTCMDFLAGS))
+	$(eval crtcmd := CRTCMD CMD($(call ESCAPE_FOR_RECIPE,$(OBJLIB))/$(basename $(@F))) srcstmf('$<') PGM($(if $(CMD_PGM_LIB),$(CMD_PGM_LIB),$(call ESCAPE_FOR_RECIPE,$(OBJLIB)))/$(CMD_PGM)) $(CRTCMDFLAGS))
 	@$(PRESETUP) \
 	$(SCRIPTSPATH)/launch "$(JOBLOGFILE)" "$(crtcmd)" "$(PRECMD)" "$(POSTCMD)" "$(notdir $@)" "$<" $(logFile)> $(logFile) 2>&1 && $(call logSuccess,$@) || $(call logFail,$@);
 endef
